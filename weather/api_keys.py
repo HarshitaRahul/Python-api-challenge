31ad37591be9f9a410f945a87c75e594f9928b68
HarshitaRahul <84482376+HarshitaRahul@users.noreply.github.com>
2021-07-22 23:44:01 -0400

    Update api_keys.py

--- weather/api_keys.py
+++ weather/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "92959abc2ba26e2d3e73b30d172ab32b"
+weather_api_key = "Key"
 
 # Google API Key
-g_key = "AIzaSyC6s3CFdZ9AiPa6XJWsSC1mGNXlHXNDQss"
+g_key = "Key"
